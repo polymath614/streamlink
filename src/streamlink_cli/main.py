ceaf2ee91ff5fa15e75503b61a275cb2d74e4540
beardypig <beardypig@users.noreply.github.com>
2016-11-28 15:03:59 +0000

    cli.main: make sure that streams are closed on errors

--- src/streamlink_cli/main.py
+++ src/streamlink_cli/main.py
@@ -232,9 +232,11 @@ def open_stream(stream):
         console.logger.debug("Pre-buffering 8192 bytes")
         prebuffer = stream_fd.read(8192)
     except IOError as err:
+        stream_fd.close()
         raise StreamError("Failed to read data from stream: {0}".format(err))
 
     if not prebuffer:
+        stream_fd.close()
         raise StreamError("No data returned from stream")
 
     return stream_fd, prebuffer
