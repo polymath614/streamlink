9adcccd2a96ba964db16cd27cc976b3fe6942159
back-to <back-to@users.noreply.github.com>
2017-03-26 10:45:55 +0000

    [myfreecams] Added support for custom url with user id's
    USERNAME URL = https://www.myfreecams.com/#UserName
    CUSTOM ID URL = https://www.myfreecams.com/id=01234567

--- src/streamlink/plugins/myfreecams.py
+++ src/streamlink/plugins/myfreecams.py
@@ -1,18 +1,3 @@
-"""
-https://www.myfreecams.com/_js/serverconfig.js
-- websocket_servers
-
-https://www.myfreecams.com/js/wsgw.js
-- function wsgw_connect
-- function zgw_TxLogin
-
-https://www.myfreecams.com/js/FCS.js
-FCTYPE_LOGIN = 1
-FCTYPE_USERNAMELOOKUP = 10
-FCTYPE_LOGOUT = 99
-FCVIDEO_TX_IDLE = 0
-"""
-
 import random
 import re
 
@@ -31,7 +16,9 @@ HLS_VIDEO_URL = "http://video{0}.myfreecams.com:1935/NxServer/ngrp:mfc_{1}.f4v_m
 WEBSOCKET_SERVERS = [7, 8, 9, 10, 11, 12, 20, 22, 23, 24, 25, 26, 27, 28, 29, 39]
 
 _session_re = re.compile(r"\173\04522fileno\04522\:\04522(?P<session>[\d_]+)\04522\175")
-_url_re = re.compile(r"https?\:\/\/(?:\w+\.)?myfreecams\.com\/\#(?P<username>\w+)")
+_url_re = re.compile(r"https?\:\/\/(?:\w+\.)?myfreecams\.com\/(?:\#(?P<username>\w+)|id\=(?P<user_id>\d+))")
+# USERNAME URL = https://www.myfreecams.com/#UserName
+# CUSTOM ID URL = https://www.myfreecams.com/id=01234567
 
 
 class MyFreeCams(Plugin):
@@ -42,6 +29,7 @@ class MyFreeCams(Plugin):
     def _get_streams(self):
         match = _url_re.match(self.url)
         username = match.group("username")
+        user_id = match.group("user_id")
 
         if not HAS_WEBSOCKET:
             self.logger.error("websocket-client is not installed")
@@ -50,13 +38,18 @@ class MyFreeCams(Plugin):
             self.logger.info("https://pypi.python.org/pypi/websocket-client")
             return
 
+        # https://www.myfreecams.com/_js/serverconfig.js
         xchat = "xchat{0}".format(random.choice(WEBSOCKET_SERVERS))
         ws_host = "wss://{0}.myfreecams.com/fcsl".format(xchat)
         ws = create_connection(ws_host)
 
+        # https://www.myfreecams.com/js/wsgw.js
+        # https://www.myfreecams.com/js/FCS.js
         send_msg_hello = "hello fcserver\n\0"
+        # FCTYPE_LOGIN = 1
         send_msg_login = "1 0 0 20071025 0 guest:guest\n\0"
         send_msg_ping = "1 0 0 0 0\n\0"
+        # FCTYPE_LOGOUT = 99
         send_msg_logout = "99 0 0 0 0"
 
         ws.send(send_msg_hello)
@@ -67,7 +60,7 @@ class MyFreeCams(Plugin):
         while status_regex is not True:
             if loop_number is 20:
                 # quit script after 20 trys
-                self.logger.debug("Is your connection ok?")
+                self.logger.error("Is your connection ok?")
                 return
 
             # send message to the websocket server
@@ -87,17 +80,24 @@ class MyFreeCams(Plugin):
         ws.send(send_msg_logout)
         ws.close()
 
+        if username:
+            re_uid = r"\d+"
+            re_username = username
+        elif user_id:
+            re_uid = int(user_id)
+            re_username = r"\w+"
+
         # regex for http data
         _data_channel_re = re.compile(r"""
             \"nm\"\:\"(?P<username>{0})\"\,
             [^\173\175]+
-            \"uid\"\:(?P<uid>\d+)\,
+            \"uid\"\:(?P<uid>{1})\,
             \"vs\"\:(?P<vs>\d+)\,
             [^\173\175]+
             \173
             [^\173\175]+
             \"camserv\"\:(?P<server>\d+)
-            """.format(username), re.VERBOSE | re.IGNORECASE)
+            """.format(re_username, re_uid), re.VERBOSE | re.IGNORECASE)
 
         # get data from http server
         cookies = {"cid": "3149", "gw": "1"}
@@ -106,29 +106,30 @@ class MyFreeCams(Plugin):
 
         if not data_channel:
             # abort if the regex can't find the username
-            self.logger.error("Stream is offline or username is invalid - {0}".format(username))
+            self.logger.error("Stream is offline or username/user_id is invalid")
             return
 
-        uid = int(data_channel.group("uid")) + 100000000
+        username = data_channel.group("username")
+        uid = int(data_channel.group("uid"))
+        uid_video = uid + 100000000
         vs = int(data_channel.group("vs"))
         camserver = int(data_channel.group("server"))
 
-        self.logger.debug("UID: {0}".format(uid))
-        self.logger.debug("VS - FCVIDEO: {0}".format(vs))
-        self.logger.debug("CAMSERVER: {0}".format(camserver))
+        self.logger.info("USER ID: {0}".format(uid))
+        self.logger.info("USERNAME: {0}".format(username))
+        self.logger.debug("VIDEO STATUS: {0}".format(vs))
 
         if vs is 0:
+            # FCVIDEO_TX_IDLE = 0
             if camserver >= 840:
                 server = camserver - 500
             elif camserver < 839:
                 server = 0
 
-            self.logger.debug("VIDEO SERVER: {0}".format(server))
-
             if server:
-                hls_url = HLS_VIDEO_URL.format(server, uid)
+                hls_url = HLS_VIDEO_URL.format(server, uid_video)
 
-                self.logger.debug(hls_url)
+                self.logger.debug("HLS URL: {0}".format(hls_url))
 
                 for s in HLSStream.parse_variant_playlist(self.session, hls_url).items():
                     yield s
--- tests/test_plugin_myfreecams.py
+++ tests/test_plugin_myfreecams.py
@@ -5,12 +5,20 @@ from streamlink.plugins.myfreecams import MyFreeCams
 
 class TestPluginMyFreeCams(unittest.TestCase):
     def test_can_handle_url(self):
-        # should match
+        # should match - username url
         self.assertTrue(MyFreeCams.can_handle_url("http://myfreecams.com/#USERNAME"))
         self.assertTrue(MyFreeCams.can_handle_url("http://www.myfreecams.com/#USERNAME"))
         self.assertTrue(MyFreeCams.can_handle_url("https://myfreecams.com/#USERNAME"))
         self.assertTrue(MyFreeCams.can_handle_url("https://www.myfreecams.com/#USERNAME"))
 
+        # should match - custom userid url
+        self.assertTrue(MyFreeCams.can_handle_url("http://myfreecams.com/id=61234567"))
+        self.assertTrue(MyFreeCams.can_handle_url("http://www.myfreecams.com/id=71234567"))
+        self.assertTrue(MyFreeCams.can_handle_url("https://myfreecams.com/id=81234567"))
+        self.assertTrue(MyFreeCams.can_handle_url("https://www.myfreecams.com/id=91234567"))
+
         # shouldn't match
         self.assertFalse(MyFreeCams.can_handle_url("http://www.myfreecams.com"))
         self.assertFalse(MyFreeCams.can_handle_url("https://www.myfreecams.com"))
+        self.assertFalse(MyFreeCams.can_handle_url("https://www.myfreecams.com/#"))
+        self.assertFalse(MyFreeCams.can_handle_url("https://www.myfreecams.com/id="))
